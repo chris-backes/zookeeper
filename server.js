64798d6f7483687121c50fe3a2d1baf82eb23944
Christopher Backes <Christopher.Backes@gmail.com>
2022-01-31 02:14:35 -0500

    connected APIs to front-end, deployed webpage

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 const express = require("express");
 const { animals } = require("./data/animals");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
-app.use(express.urlencoded({ extended: true}))
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("public"));
 
 function filterByQuery(query, animalsArray) {
 	let personalityTraitsArray = [];
@@ -47,30 +48,30 @@ function findById(id, animalsArray) {
 }
 
 function createNewAnimal(body, animalsArray) {
-	const animal = body
-	animalsArray.push(animal)
+	const animal = body;
+	animalsArray.push(animal);
 	fs.writeFileSync(
-		path.join(__dirname, './data/animals.json'),
-		JSON.stringify({ animals: animalsArray}, null, 2)
+		path.join(__dirname, "./data/animals.json"),
+		JSON.stringify({ animals: animalsArray }, null, 2)
 	);
 	return animal;
 }
 
 function validateAnimal(animal) {
-	if (!animal.name || typeof animal.name !== 'string') {
-	  return false;
+	if (!animal.name || typeof animal.name !== "string") {
+		return false;
 	}
-	if (!animal.species || typeof animal.species !== 'string') {
-	  return false;
+	if (!animal.species || typeof animal.species !== "string") {
+		return false;
 	}
-	if (!animal.diet || typeof animal.diet !== 'string') {
-	  return false;
+	if (!animal.diet || typeof animal.diet !== "string") {
+		return false;
 	}
 	if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
-	  return false;
+		return false;
 	}
 	return true;
-  }
+}
 
 app.get("/api/animals", (req, res) => {
 	let results = animals;
@@ -93,19 +94,35 @@ app.post("/api/animals", (req, res) => {
 	req.body.id = animals.length.toString();
 
 	if (!validateAnimal(req.body)) {
-		res.status(400).send('The animal is nor properyl formatted.');
+		res.status(400).send("The animal is nor properyl formatted.");
 	} else {
 		const animal = createNewAnimal(req.body, animals);
 		res.json(animal);
 	}
 
-	  if (!validateAnimal(req.body)) {
-		res.status(400).send('The animal is not properly formatted.');
-	  } else {
+	if (!validateAnimal(req.body)) {
+		res.status(400).send("The animal is not properly formatted.");
+	} else {
 		const animal = createNewAnimal(req.body, animals);
 		res.json(animal);
-	  }
+	}
 });
+
+app.get("/", (req, res) => {
+	res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+app.get("/animals", (req, res) => {
+	res.sendFile(path.join(__dirname, "./public/animals.html"));
+});
+
+app.get("/zookeepers", (req, res) => {
+	res.sendFile(path.join(__dirname, "./public/zookeepers.html"));
+});
+
+app.get('*', (req, res) => {
+	res.sendFile(path.join(__dirname, './public/index.html'))
+})
 
 app.listen(PORT, () => {
 	console.log(`API server now on port ${PORT}!`);
